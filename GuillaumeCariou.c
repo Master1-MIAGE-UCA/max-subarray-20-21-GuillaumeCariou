0fbff8034588f558d500f6c34bd2a9f8428e8637
GuillaumeCariou <guillaume.cariou06@gmail.com>
2021-01-12 23:03:02 +0100

    optimisation de la copie de code

--- GuillaumeCariou.c
+++ GuillaumeCariou.c
@@ -99,153 +99,18 @@ struct tablo * sum_prefix(struct tablo source) {//==============================
   b->size = final->size;
   b->tab = final->tab;
 
-
-  printArray(final);
   return final;
 }
 
 
 
 struct tablo * sum_suffix(struct tablo source) {//=========================================================================================OK
-  //montee
-  struct tablo * a = malloc(sizeof(struct tablo));
-  a->tab = malloc(source.size*2*sizeof(int));
-  a->size =source.size*2;
-  a->tab[0] = 0;
-
   struct tablo * copy = reverseArray(&source);
-
-  #pragma omp parallel for
-	for (int i = copy->size-1; 0 <= i; i--)
-  {
-    a->tab[i+copy->size] = copy->tab[i];
-  }
-
-  int m = log(copy->size)/log(2);
-
-  for (int i = m -1; 0 <= i; i--){
-    int inf = pow(2,i);
-    int sup = pow(2, i + 1) - 1;
-
-    #pragma omp parallel for
-    for (int j = inf; j <= sup; j++){
-      a->tab[j] = a->tab[j*2] + a->tab[(j*2)+1];
-    }
-  }
-
-  //descente
-  struct tablo * b = malloc(sizeof(struct tablo));
-  b->tab= malloc(source.size*2*sizeof(int));
-  b->size=source.size*2;
-  b->tab[0] = 0;
-
-
-  m = log(a->size/2)/log(2);
-  b->tab[1] = 0;
-
-  for (int i = 1; i <= m; i++){
-    int inf = pow(2,i);
-    int sup = pow(2, i + 1) - 1;
-
-    #pragma omp parallel for
-    for (int j = inf; j <= sup; j++){
-      if(j%2 == 0){//fils gauche
-        b->tab[j] = b->tab[j/2];
-      }else{//fils droit
-        b->tab[j] = b->tab[j/2] + a->tab[j-1];
-      }
-    }
-  }
-
-  //final
-  struct tablo * final = malloc(sizeof(struct tablo));
-  final->size = a->size/2;
-  final->tab = malloc(final->size*sizeof(int));
-
-  #pragma omp parallel for
-  for (int i = 0; i < final->size; i++){
-    final->tab[i] = a->tab[i+final->size] + b->tab[i+final->size];
-  }
-  
-  b->size = final->size;
-  b->tab = final->tab;
-  
-  final = reverseArray(final);
-
-  printArray(final);
-  return final;
+  copy = sum_prefix(*copy);
+  copy = reverseArray(copy);
+  return copy;
 }
 
-
-struct tablo * max_suffix(struct tablo source) {//=========================================================================================
-  //montee
-  struct tablo * a = malloc(sizeof(struct tablo));
-  a->tab = malloc(source.size*2*sizeof(int));
-  a->size =source.size*2;
-  a->tab[0] = 0;
-
-  struct tablo * copy = reverseArray(&source);
-
-  #pragma omp parallel for
-	for (int i = copy->size-1; 0 <= i; i--)
-  {
-    a->tab[i+copy->size] = copy->tab[i];
-  }
-
-  int m = log(copy->size)/log(2);
-
-  for (int i = m -1; 0 <= i; i--){
-    int inf = pow(2,i);
-    int sup = pow(2, i + 1) - 1;
-
-    #pragma omp parallel for
-    for (int j = inf; j <= sup; j++){
-      a->tab[j] = fmax(a->tab[j*2], a->tab[(j*2)+1]);
-    }
-  }
-
-  //descente
-  struct tablo * b = malloc(sizeof(struct tablo));
-  b->tab= malloc(source.size*2*sizeof(int));
-  b->size=source.size*2;
-  b->tab[0] = 0;
-
-
-  m = log(a->size/2)/log(2);
-  b->tab[1] = 0;
-
-  for (int i = 1; i <= m; i++){
-    int inf = pow(2,i);
-    int sup = pow(2, i + 1) - 1;
-
-    #pragma omp parallel for
-    for (int j = inf; j <= sup; j++){
-      if(j%2 == 0){//fils gauche
-        b->tab[j] = b->tab[j/2];
-      }else{//fils droit
-        b->tab[j] = fmax(b->tab[j/2], a->tab[j-1]);
-      }
-    }
-  }
-
-  //final
-  struct tablo * final = malloc(sizeof(struct tablo));
-  final->size = a->size/2;
-  final->tab = malloc(final->size*sizeof(int));
-
-  #pragma omp parallel for
-  for (int i = 0; i < final->size; i++){
-    final->tab[i] = fmax(a->tab[i+final->size], b->tab[i+final->size]);
-  }
-  
-  b->size = final->size;
-  b->tab = final->tab;
-  
-  final = reverseArray(final);
-
-  printArray(final);
-  return final;
-}
 
 
 struct tablo * max_prefix(struct tablo source) {//=========================================================================================OK
@@ -310,9 +175,16 @@ struct tablo * max_prefix(struct tablo source) {//==============================
   b->size = final->size;
   b->tab = final->tab;
 
-
-  printArray(final);
   return final;
+}
+
+
+
+struct tablo * max_suffix(struct tablo source) {//=========================================================================================OK
+  struct tablo * copy = reverseArray(&source);
+  copy = max_prefix(*copy);
+  copy = reverseArray(copy);
+  return copy;
 }
 
 
@@ -346,10 +218,19 @@ int main(int argc, char **argv) {
   char line[MAXCHAR];
   fgets(line,MAXCHAR,f);
   struct tablo * origine = foo(line);
+
   struct tablo * PSUM = sum_prefix(*origine);
+  printf("===sum_prefix===");
+  printArray(PSUM);
   struct tablo * SSUM = sum_suffix(*origine);
+  printf("===sum_suffix===");
+  printArray(SSUM);
   struct tablo * SMAX = max_suffix(*PSUM);
+  printf("===max_suffix===");
+  printArray(SMAX);
   struct tablo * PMAX = max_prefix(*SSUM);
+  printf("===max_prefix===");
+  printArray(PMAX);
 
   //continuer étape 5
 }
